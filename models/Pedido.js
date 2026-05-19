9f1cc064b3c627b8d60f5164ef909621621d9200
Vitor Perrone <vitinhoperrone@gmail.com>
2026-05-19 01:02:31 -0300

    update: corrigindo schemas do banco de dados e whatsapp

--- models/Pedido.js
+++ models/Pedido.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
     modelo: { type: String, required: true },
-    tecido: { type: String, required: true },
+    cor: { type: String, required: true },
     tamanho: { type: String, required: true },
     quantidade: { type: Number, required: true, default: 1 },
     preco: { type: Number, required: true },
-    pronto: { type: Boolean, default: false }
+    pronto: { type: Boolean, default: false },
+    isProntaEntrega: { type: Boolean, default: false }
 }, { _id: true }); // Mudei para _id: true para podermos dar "check" em itens específicos
 
 const pedidoSchema = new mongoose.Schema({
+    pedidoId: { type: String, required: true },
     nome: { type: String, required: true },
     telefone: { type: String, required: true },
     itens: [itemSchema], // Lista de camisas
